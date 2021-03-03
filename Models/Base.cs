4bf15bb899716e100e90dde5c297c3cb7a834d89
Angel Arsenio Reyes Espinal <angel.reyes@solvex.com.do>
2021-03-02 21:08:16 -0400

    commit

--- Models/Base.cs
+++ Models/Base.cs
@@ -8,12 +8,13 @@ namespace SistemaDeCompras.Models
     public interface IBase 
     { 
         public int? Id { get; set; }
-        public int Estado { get; set; }
+        public bool Estado { get; set; }
     }
 
     public class Base : IBase
     {
+        public Base() => Estado = true;
         public int? Id { get; set; }
-        public int Estado { get; set; }
+        public bool Estado { get; set; }
     }
 }
--- Models/OrdenDeCompra.cs
+++ Models/OrdenDeCompra.cs
@@ -11,9 +11,16 @@ namespace SistemaDeCompras.Models
         public DateTime FechaOrden { get; set; }
         public int Cantidad { get; set; }
         public decimal CostoUnitario { get; set; }
+
         public int ArticuloId { get; set; }
         public int UnidadDeMedidaId { get; set; }
+        public int ProveedorId { get; set; }
+        public int DepartamentoId { get; set; }
+
         public virtual UnidadDeMedida UnidadDeMedida { get; set; }
         public virtual Articulo Articulo { get; set; }
+        public virtual Proveedor Proveedor { get; set; }
+        public virtual Departamento Departamento { get; set; }
+
     }
 }
